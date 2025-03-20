e7b2ba85c4b4c0e990df0880000c29b2ec5e3ffc
rakhi120805 <151828091+rakhi120805@users.noreply.github.com>
2025-03-20 06:44:21 +0530

    Update and rename first.c to Thread.c

--- Thread.c
+++ Thread.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <pthread.h>
 
-// Function the thread will run
+
 void* thread_function(void* arg) {
     printf("Hello from the thread!\n");
     return NULL;
 }
 
 int main() {
-    pthread_t thread; // Thread handle
+    pthread_t thread;
     int result;
 
-    // Create the thread
+   
     result = pthread_create(&thread, NULL, thread_function, NULL);
     if (result != 0) {
         printf("Error creating thread\n");
         return 1;
     }
 
-    // Wait for the thread to finish
+  
     pthread_join(thread, NULL);
     printf("Thread finished, back in main!\n");
 
