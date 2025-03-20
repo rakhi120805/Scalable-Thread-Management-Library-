bd706079fe34de072af847cdfdd4360112717bd1
rakhi120805 <151828091+rakhi120805@users.noreply.github.com>
2025-03-20 20:09:47 +0530

    Update thread_manager.c

--- thread_manager.c
+++ thread_manager.c
@@ -3,20 +3,19 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define MAX_THREADS 4
 #define MAX_TASKS 10
 
-
 typedef struct {
-    void (*function)(void*); 
-    void* arg;             
-    int priority;           
+    void (*function)(void*);
+    void* arg;
+    int priority;
+    void (*callback)(void*); // Callback on completion
 } Task;
 
-
 typedef struct {
-    pthread_t threads[MAX_THREADS];
-    int busy[MAX_THREADS];
+    pthread_t* threads;
+    int* busy;
+    int thread_count;
     Task tasks[MAX_TASKS];
     int task_count;
     int running;
@@ -24,12 +23,11 @@ typedef struct {
     pthread_cond_t cond;
 } ThreadPool;
 
-
 void* worker(void* arg) {
     ThreadPool* pool = (ThreadPool*)arg;
     int id = -1;
     pthread_mutex_lock(&pool->mutex);
-    for (int i = 0; i < MAX_THREADS; i++) {
+    for (int i = 0; i < pool->thread_count; i++) {
         if (pthread_self() == pool->threads[i]) id = i;
     }
     pthread_mutex_unlock(&pool->mutex);
@@ -58,69 +56,74 @@ void* worker(void* arg) {
         pool->task_count--;
         pool->busy[id] = 1;
         pthread_mutex_unlock(&pool->mutex);
-        if (has_task) task.function(task.arg);
+        if (has_task) {
+            task.function(task.arg);
+            if (task.callback) task.callback(task.arg);
+        }
     }
     return NULL;
 }
 
-
-ThreadPool* create_thread_pool() {
+ThreadPool* create_thread_pool(int num_threads) {
     ThreadPool* pool = malloc(sizeof(ThreadPool));
+    pool->thread_count = num_threads;
+    pool->threads = malloc(sizeof(pthread_t) * num_threads);
+    pool->busy = malloc(sizeof(int) * num_threads);
     pool->task_count = 0;
     pool->running = 1;
     pthread_mutex_init(&pool->mutex, NULL);
     pthread_cond_init(&pool->cond, NULL);
-
-    for (int i = 0; i < MAX_THREADS; i++) {
+    for (int i = 0; i < num_threads; i++) {
         pool->busy[i] = 0;
         pthread_create(&pool->threads[i], NULL, worker, pool);
-        pthread_detach(pool->threads[i]); 
+        pthread_detach(pool->threads[i]);
     }
     return pool;
 }
 
-
-void add_task(ThreadPool* pool, void (*function)(void*), void* arg, int priority) {
+void add_task(ThreadPool* pool, void (*function)(void*), void* arg, int priority, void (*callback)(void*)) {
     pthread_mutex_lock(&pool->mutex);
     if (pool->task_count < MAX_TASKS) {
         pool->tasks[pool->task_count].function = function;
         pool->tasks[pool->task_count].arg = arg;
         pool->tasks[pool->task_count].priority = priority;
+        pool->tasks[pool->task_count].callback = callback;
         pool->task_count++;
-        pthread_cond_signal(&pool->cond); 
+        pthread_cond_signal(&pool->cond);
     }
     pthread_mutex_unlock(&pool->mutex);
 }
-
 
 void destroy_thread_pool(ThreadPool* pool) {
     pthread_mutex_lock(&pool->mutex);
     pool->running = 0;
     pthread_cond_broadcast(&pool->cond);
     pthread_mutex_unlock(&pool->mutex);
-
     pthread_mutex_destroy(&pool->mutex);
     pthread_cond_destroy(&pool->cond);
+    free(pool->threads);
+    free(pool->busy);
     free(pool);
 }
+
 void sample_task(void* arg) {
-    int id = (int)(long)arg;
-    printf("Task %d running with priority %d\n", id, id);
+    printf("Task %ld running with priority %d\n", (long)arg, (int)(long)arg);
     sleep(1);
 }
 
-int main() {
-    ThreadPool* pool = create_thread_pool();
-    
-    
-    add_task(pool, sample_task, (void*)1, 1);
-    add_task(pool, sample_task, (void*)2, 0);
-    add_task(pool, sample_task, (void*)3, 2);
+void sample_callback(void* arg) {
+    printf("Task %ld completed\n", (long)arg);
+}
 
-    sleep(5); 
+int main() {
+    ThreadPool* pool = create_thread_pool(3);
+    add_task(pool, sample_task, (void*)1, 1, sample_callback);
+    add_task(pool, sample_task, (void*)2, 0, sample_callback);
+    add_task(pool, sample_task, (void*)3, 2, sample_callback);
+    sleep(5);
     printf("Busy threads: ");
-    for(int i=0;i<MAX_THREADS;i++){
-        if(pool->busy[i])printf("%d ",i);
+    for (int i = 0; i < pool->thread_count; i++) {
+        if (pool->busy[i]) printf("%d ", i);
     }
     printf("\n");
     destroy_thread_pool(pool);
