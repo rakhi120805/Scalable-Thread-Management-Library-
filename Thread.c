07c2116697e776dcb055111a5b55f8a88d4223de
rakhi120805 <151828091+rakhi120805@users.noreply.github.com>
2025-03-22 18:38:17 +0530

    Update Thread.c

--- Thread.c
+++ Thread.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 #include <pthread.h>
-
+#include "Thread.h"
 
 void* thread_function(void* arg) {
     printf("Hello from the thread!\n");
     return NULL;
 }
 
-int main() {
+void run_basic_thread_test() {
     pthread_t thread;
-    int result;
-
-   
-    result = pthread_create(&thread, NULL, thread_function, NULL);
+    int result = pthread_create(&thread, NULL, thread_function, NULL);
     if (result != 0) {
         printf("Error creating thread\n");
-        return 1;
+        return;
     }
-
-  
     pthread_join(thread, NULL);
     printf("Thread finished, back in main!\n");
-
-    return 0;
 }
