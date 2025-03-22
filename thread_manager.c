938ba6943122d5ce2d7406c0690f0813b7f8cd43
rakhi120805 <151828091+rakhi120805@users.noreply.github.com>
2025-03-22 18:44:19 +0530

    Update thread_manager.c

--- thread_manager.c
+++ thread_manager.c
@@ -2,95 +2,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <time.h>
+#include "Thread.h"
+#include "MessageQueue.h"
+#include "Performance.h"
 
-#define MAX_TASKS 10
-#define TIMEOUT_SEC 2
-
-typedef struct {
-    void (*function)(void*);
-    void* arg;
-    int priority;
-    void (*callback)(void*);
-    time_t start_time;
-    int timed_out;
-} Task;
+#define NUM_CONSUMERS 3
+#define PREDEFINED_TASKS 10
 
 typedef struct {
     pthread_t* threads;
     int* busy;
     int thread_count;
-    Task tasks[MAX_TASKS];
-    int task_count;
-    int running;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-    pthread_t timeout_thread;
+    MessageQueue queue;
+    PerformanceMetrics metrics;
 } ThreadPool;
-
-void* timeout_checker(void* arg) {
-    ThreadPool* pool = (ThreadPool*)arg;
-    while (1) {
-        pthread_mutex_lock(&pool->mutex);
-        if (!pool->running && pool->task_count == 0) {
-            pthread_mutex_unlock(&pool->mutex);
-            return NULL;
-        }
-        for (int i = 0; i < pool->task_count; i++) {
-            if (time(NULL) - pool->tasks[i].start_time > TIMEOUT_SEC) {
-                pool->tasks[i].timed_out = 1;
-                printf("Task %ld timed out\n", (long)pool->tasks[i].arg);
-                for (int j = i + 1; j < pool->task_count; j++) {
-                    pool->tasks[j - 1] = pool->tasks[j];
-                }
-                pool->task_count--;
-                i--;
-            }
-        }
-        pthread_mutex_unlock(&pool->mutex);
-        sleep(1);
-    }
-    return NULL;
-}
 
 void* worker(void* arg) {
     ThreadPool* pool = (ThreadPool*)arg;
     int id = -1;
-    pthread_mutex_lock(&pool->mutex);
+    pthread_mutex_lock(&pool->queue.mutex);
     for (int i = 0; i < pool->thread_count; i++) {
         if (pthread_self() == pool->threads[i]) id = i;
     }
-    pthread_mutex_unlock(&pool->mutex);
+    pthread_mutex_unlock(&pool->queue.mutex);
 
     while (1) {
-        Task task;
-        int has_task = 0;
-        pthread_mutex_lock(&pool->mutex);
-        while (pool->task_count == 0 && pool->running) {
-            pool->busy[id] = 0;
-            pthread_cond_wait(&pool->cond, &pool->mutex);
-        }
-        if (!pool->running && pool->task_count == 0) {
-            pthread_mutex_unlock(&pool->mutex);
-            return NULL;
-        }
-        int max_idx = 0;
-        for (int i = 1; i < pool->task_count; i++) {
-            if (pool->tasks[i].priority > pool->tasks[max_idx].priority) max_idx = i;
-        }
-        task = pool->tasks[max_idx];
-        has_task = 1;
-        for (int i = max_idx + 1; i < pool->task_count; i++) {
-            pool->tasks[i - 1] = pool->tasks[i];
-        }
-        pool->task_count--;
+        Task *task = pop_message(&pool->queue);
+        if (task == NULL) break;
         pool->busy[id] = 1;
-        task.start_time = time(NULL);
-        pthread_mutex_unlock(&pool->mutex);
-        if (has_task && !task.timed_out) {
-            task.function(task.arg);
-            if (task.callback) task.callback(task.arg);
+        if (!task->timed_out) {
+            task->function(task->arg);
+            if (task->callback) task->callback(task->arg);
         }
+        record_task_completion(&pool->metrics);
+        pool->busy[id] = 0;
+        free(task);
     }
     return NULL;
 }
@@ -100,42 +46,18 @@ ThreadPool* create_thread_pool(int num_threads) {
     pool->thread_count = num_threads;
     pool->threads = malloc(sizeof(pthread_t) * num_threads);
     pool->busy = malloc(sizeof(int) * num_threads);
-    pool->task_count = 0;
-    pool->running = 1;
-    pthread_mutex_init(&pool->mutex, NULL);
-    pthread_cond_init(&pool->cond, NULL);
+    init_message_queue(&pool->queue);
+    init_performance_metrics(&pool->metrics);
     for (int i = 0; i < num_threads; i++) {
         pool->busy[i] = 0;
         pthread_create(&pool->threads[i], NULL, worker, pool);
         pthread_detach(pool->threads[i]);
     }
-    pthread_create(&pool->timeout_thread, NULL, timeout_checker, pool);
-    pthread_detach(pool->timeout_thread);
     return pool;
 }
 
-void add_task(ThreadPool* pool, void (*function)(void*), void* arg, int priority, void (*callback)(void*)) {
-    pthread_mutex_lock(&pool->mutex);
-    if (pool->task_count < MAX_TASKS) {
-        pool->tasks[pool->task_count].function = function;
-        pool->tasks[pool->task_count].arg = arg;
-        pool->tasks[pool->task_count].priority = priority;
-        pool->tasks[pool->task_count].callback = callback;
-        pool->tasks[pool->task_count].start_time = 0;
-        pool->tasks[pool->task_count].timed_out = 0;
-        pool->task_count++;
-        pthread_cond_signal(&pool->cond);
-    }
-    pthread_mutex_unlock(&pool->mutex);
-}
-
 void destroy_thread_pool(ThreadPool* pool) {
-    pthread_mutex_lock(&pool->mutex);
-    pool->running = 0;
-    pthread_cond_broadcast(&pool->cond);
-    pthread_mutex_unlock(&pool->mutex);
-    pthread_mutex_destroy(&pool->mutex);
-    pthread_cond_destroy(&pool->cond);
+    shutdown_message_queue(&pool->queue);
     free(pool->threads);
     free(pool->busy);
     free(pool);
@@ -143,7 +65,7 @@ void destroy_thread_pool(ThreadPool* pool) {
 
 void sample_task(void* arg) {
     printf("Task %ld running with priority %d\n", (long)arg, (int)(long)arg);
-    sleep(3); 
+    sleep(3);
 }
 
 void sample_callback(void* arg) {
@@ -151,16 +73,34 @@ void sample_callback(void* arg) {
 }
 
 int main() {
-    ThreadPool* pool = create_thread_pool(3);
-    add_task(pool, sample_task, (void*)1, 1, sample_callback);
-    add_task(pool, sample_task, (void*)2, 0, sample_callback);
-    add_task(pool, sample_task, (void*)3, 2, sample_callback);
-    sleep(5);
+
+    printf("Running basic thread test...\n");
+    run_basic_thread_test();
+
+
+    ThreadPool* pool = create_thread_pool(NUM_CONSUMERS);
+    start_timer(&pool->metrics);
+
+    printf("Adding predefined tasks...\n");
+    for (int i = 0; i < PREDEFINED_TASKS; i++) {
+        int *num = (int *)malloc(sizeof(int));
+        *num = i + 1;
+        push_message(&pool->queue, sample_task, num, i % 3, sample_callback);
+        printf("Added Task %d with priority %d\n", i + 1, i % 3);
+        usleep(100000);
+    }
+
+  
+    sleep(10);
     printf("Busy threads: ");
     for (int i = 0; i < pool->thread_count; i++) {
         if (pool->busy[i]) printf("%d ", i);
     }
     printf("\n");
+
+    stop_timer(&pool->metrics);
+    print_performance_metrics(&pool->metrics);
+
     destroy_thread_pool(pool);
     printf("Thread pool shut down\n");
     return 0;
